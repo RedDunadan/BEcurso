d25e358e2cad1e45dfddaaa2fc63cf16eafada20
RedDunadan <devrhedd@gmail.com>
2025-09-24 15:37:11 -0300

    iterando

--- Clase 5/index.js
+++ Clase 5/index.js
@@ -1,15 +1,18 @@
-import { loadDB , saveDB } from "./persistencia";
+const { loadDB , saveDB } = "./persistencia";
 const args = process.argv.slice(2);
 const command = args[0];
-const value = args[1];
+
+const store = loadDB();
 
 switch (command) {
     case 'post':
+        const value = args[1];
         if (!value) {
             console.log('Please provide a value to post.');
             break;
         }
         store.push(value);
+        saveDB(store);
         console.log(`Posted: ${value}`);
         break;
     case 'get':
@@ -18,11 +21,12 @@ switch (command) {
     case 'put':
         const putIndex = parseInt(args[1], 10);
         const putValue = args[2];
-        if (isNaN(putIndex) || !putValue) {
+        if (isNaN(putIndex) || putValue === undefined) {
             console.log('Please provide a valid index and value to put.');
             break;
-        } if (store[putIndex]) {
+        } if (putIndex >= 0 && putIndex < store.length) {
             store[putIndex] = putValue;
+            saveDB(store);
             console.log(`Updated index ${putIndex} with value: ${putValue}`);
             break;
         } else {
